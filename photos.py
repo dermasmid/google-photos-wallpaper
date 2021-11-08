92da25eb130f233fca01ca0f4137414cbc335f00
Cheskel Twersky <twerskycheskel@gmail.com>
2021-11-08 12:39:40 +0200

    blacken

--- photos.py
+++ photos.py
@@ -9,19 +9,18 @@ import signal
 from threading import Event
 
 
-
-ALBUM_ID = os.environ.get('ALBUM_ID')
-SLEEP_MINS = int(os.environ.get('SLEEP_MINS'))
-SAVE_PHOTOS = bool(os.environ.get('SAVE_PHOTOS'))
-WALLPAPER_DIR = f'{os.getcwd()}/wallpapers'
-
+ALBUM_ID = os.environ.get("ALBUM_ID")
+SLEEP_MINS = int(os.environ.get("SLEEP_MINS"))
+SAVE_PHOTOS = bool(os.environ.get("SAVE_PHOTOS"))
+WALLPAPER_DIR = f"{os.getcwd()}/wallpapers"
 
 
-
-scope = google_workspace.types.Scope('https://www.googleapis.com/auth/photoslibrary.readonly', '', '')
-service = google_workspace.service.GoogleService('photoslibrary', scopes= [scope], version= 'v1')
-
-
+scope = google_workspace.types.Scope(
+    "https://www.googleapis.com/auth/photoslibrary.readonly", "", ""
+)
+service = google_workspace.service.GoogleService(
+    "photoslibrary", scopes=[scope], version="v1"
+)
 
 
 def main(last_photo: str = None):
@@ -31,27 +30,38 @@ def main(last_photo: str = None):
     while True:
 
         # get the album from google photos
-        album_data = service.mediaItems().search(body= {"albumId": ALBUM_ID, "pageToken": next_page_token}).execute()
+        album_data = (
+            service.mediaItems()
+            .search(body={"albumId": ALBUM_ID, "pageToken": next_page_token})
+            .execute()
+        )
 
-        photos = album_data.get('mediaItems', [])
+        photos = album_data.get("mediaItems", [])
         for photo in photos:
-            photo_id = photo.get('id')
+            photo_id = photo.get("id")
             if not last_photo:
-                file_path = f'{WALLPAPER_DIR}/{photo_id}'
+                file_path = f"{WALLPAPER_DIR}/{photo_id}"
 
                 # if file is not on disk, download
                 if not os.path.exists(file_path):
-                    download_photo(photo_id, photo.get('baseUrl'))
-                
+                    download_photo(photo_id, photo.get("baseUrl"))
+
                 # set as wallpaper
-                subprocess.run(['gsettings', 'set', 'org.gnome.desktop.background', 'picture-uri', f'file:///{file_path}'])
+                subprocess.run(
+                    [
+                        "gsettings",
+                        "set",
+                        "org.gnome.desktop.background",
+                        "picture-uri",
+                        f"file:///{file_path}",
+                    ]
+                )
 
                 # delete old photo if not save photos
                 if not SAVE_PHOTOS:
                     most_recent_photo.append(file_path)
                     if len(most_recent_photo) > 1:
                         os.remove(most_recent_photo.pop(0))
-
 
                 stopper.exit.wait(SLEEP_MINS * 60)
 
@@ -62,46 +72,38 @@ def main(last_photo: str = None):
             else:
                 if last_photo == photo_id:
                     last_photo = None
-                    most_recent_photo.append(f'{WALLPAPER_DIR}/{photo_id}')
-            
-            
+                    most_recent_photo.append(f"{WALLPAPER_DIR}/{photo_id}")
 
-        next_page_token = album_data.get('nextPageToken')
+        next_page_token = album_data.get("nextPageToken")
 
         # Handle Stop
         if stopper.exit.is_set():
-            with open('state.json', 'w') as f:
-                json.dump({'last_photo': photo_id}, f)
+            with open("state.json", "w") as f:
+                json.dump({"last_photo": photo_id}, f)
             break
 
 
 def download_photo(photo_id: str, url: str):
-    data = requests.get(f'{url}=w1500-h1000').content
-    with open(f'{WALLPAPER_DIR}/{photo_id}', "wb") as f:
+    data = requests.get(f"{url}=w1500-h1000").content
+    with open(f"{WALLPAPER_DIR}/{photo_id}", "wb") as f:
         f.write(data)
 
 
-
 class GracefulStop:
-
-
     def __init__(self):
         self.exit = Event()
         signal.signal(signal.SIGINT, self.quit)
         signal.signal(signal.SIGTERM, self.quit)
 
-
     def quit(self, signum, frame):
         self.exit.set()
 
 
-
-
 if __name__ == "__main__":
-    with open('state.json', 'r') as f:
+    with open("state.json", "r") as f:
         state_data = json.load(f)
     if state_data:
-        last_photo = state_data['last_photo']
+        last_photo = state_data["last_photo"]
     else:
         last_photo = None
-    main(last_photo= last_photo)
+    main(last_photo=last_photo)
--- select_album.py
+++ select_album.py
@@ -3,9 +3,12 @@ import google_workspace
 import os
 
 
-scope = google_workspace.types.Scope('https://www.googleapis.com/auth/photoslibrary.readonly', '', '')
-service = google_workspace.service.GoogleService('photoslibrary', scopes= [scope], version= 'v1')
-
+scope = google_workspace.types.Scope(
+    "https://www.googleapis.com/auth/photoslibrary.readonly", "", ""
+)
+service = google_workspace.service.GoogleService(
+    "photoslibrary", scopes=[scope], version="v1"
+)
 
 
 def main():
@@ -14,34 +17,34 @@ def main():
     next_page_token = None
     done = False
     while not done:
-        albums_data = service.albums().list(pageSize= 50, pageToken= next_page_token).execute()
-        albums = albums_data.get('albums')
+        albums_data = (
+            service.albums().list(pageSize=50, pageToken=next_page_token).execute()
+        )
+        albums = albums_data.get("albums")
         for album in albums:
             albums_dict[str(index)] = {
-                'title': album.get('title'),
-                'id': album.get('id'),
-                'url': album.get('productUrl')
+                "title": album.get("title"),
+                "id": album.get("id"),
+                "url": album.get("productUrl"),
             }
             index += 1
-        next_page_token = albums_data.get('nextPageToken')
+        next_page_token = albums_data.get("nextPageToken")
 
         if not next_page_token:
             done = True
 
-
     for album_index in albums_dict:
-        print(f'{album_index}: {albums_dict[album_index]["title"]}    ({albums_dict[album_index]["url"]})')
-    selected_album_index = input('Please enter the number of the album you want: ')
+        print(
+            f'{album_index}: {albums_dict[album_index]["title"]}    ({albums_dict[album_index]["url"]})'
+        )
+    selected_album_index = input("Please enter the number of the album you want: ")
 
     if not selected_album_index in albums_dict:
-        raise ValueError('invalid selection!!')
+        raise ValueError("invalid selection!!")
 
     print(f'You selected: {albums_dict[selected_album_index]["title"]}')
-    with open('enviroment.env', 'a') as e:
+    with open("enviroment.env", "a") as e:
         e.write(f'ALBUM_ID={albums_dict[selected_album_index]["id"]}')
-
-
-
 
 
 if __name__ == "__main__":
